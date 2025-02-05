ff115c6ee21037ef3e0666a7a1a501568f65020b
Oksana Cherviachenko <kvintinovna@gmail.com>
2025-02-05 09:22:48 +0100

    krok 2.1 2.2 done

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { getContactsController, getContactByIdController, createContactController, deleteContactController, patchContactController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js"; 
+import { createContactSchema } from "../validation/contacts.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { updateContactSchema } from "../validation/contacts.js";
 
 const router = Router();
 
@@ -8,10 +11,10 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 
   router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 export default router;
